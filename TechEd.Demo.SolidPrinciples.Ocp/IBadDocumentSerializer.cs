24011c721835214a71bd864dd6b97ead20695f61
Chandler Fang <chandlerfang@gmail.com>
2016-06-05 21:02:40 +1000

    Project Initial.

---
+++ TechEd.Demo.SolidPrinciples.Ocp/IBadDocumentSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechEd.Demo.SolidPrinciples.Ocp
+{
+    interface IBadDocumentSerializer
+    {
+
+    }
+
+    interface IAkaDocumentSerializer : IDocumentSerializer, IBadDocumentSerializer
+    {
+         
+    }
+}
